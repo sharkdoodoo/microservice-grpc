1410e3d2146b9d59522ebb16eefecae6c93bb301
cjianlong <cjianlong@tcl.com>
2020-11-10 08:49:01 +0800

    使用grpc-spring-boot-starter 来构造客户端请求

---
+++ grpc-microservice-client/src/main/java/org/jian/grpc/microservice/ClientService.java
@@ -0,0 +1,20 @@
+package org.jian.grpc.microservice;
+
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.jian.grpc.proto.HelloReply;
+import org.jian.grpc.proto.HelloRequest;
+import org.jian.grpc.proto.SimpleGrpc;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientService {
+
+
+    @GrpcClient("local-grpc-server")
+    private SimpleGrpc.SimpleBlockingStub stub;
+
+    public String request(String name) {
+        HelloReply reply = stub.sayHello(HelloRequest.newBuilder().setName("[SpringBoot]"+name).build());
+        return reply.getMessage();
+    }
+}
